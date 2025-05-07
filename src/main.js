8d11bd2272e452054ed90c15e26db75fe2fdb2cf
Your Name <you@example.com>
2025-05-07 10:00:38 +0200

    second-commit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
-console.log("Hola mundo")
+//alert("HOLA?")
+
+// Todo List
+let newTask = document.getElementById("task")
+let addButton = document.getElementById("addButton")
+let taskList = document.getElementById("taskList")
+
+function saveTask(task) {
+    console.log(task)
+    // añadir a taskList una linea al final con la nueva tarea
+} 
+
+function addClickHandler() {
+    saveTask(newTask.value)
+    newTask.value = ""
+
+    return true
+}
+
+
+
+addButton.addEventListener("click", addClickHandler)
+
+//alert("El resultado es: ", newTask)
